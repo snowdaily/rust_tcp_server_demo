82f605b72603d6807caf56cb287bbcf0ded94007
gary <snow.daily@gmail.com>
2021-08-13 16:12:58 +0800

    Add comment.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
+    // 設置監聽接口及端口
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    // 處理請求事件
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        // println!("Connection established!");
+        // 取得 html 內容並 render 至畫面上
         handle_connection(stream);
     }
 }
 
+// 取得 html 內容並 render 至畫面上
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
@@ -31,6 +34,7 @@ fn handle_connection(mut stream: TcpStream) {
     stream.flush().unwrap();
 }
 
+// 取得 html 內容
 fn get_html_string() -> String {
     let contents = match fs::read_to_string("hello.html") {
         Ok(contents) => contents,

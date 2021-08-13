7823a73b6b5252260fadf718fd5046405a86225b
gary <snow.daily@gmail.com>
2021-08-13 16:06:15 +0800

    Init.

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // println!("Connection established!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let contents = get_html_string();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn get_html_string() -> String {
+    let contents = match fs::read_to_string("hello.html") {
+        Ok(contents) => contents,
+        Err(_e) => String::from("")
+    };
+
+    return contents;
+}
